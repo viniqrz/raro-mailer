917211144458c0ca6599307ba39d833d5da6d819
Rogerio Cezar Fertonani <rogeriocfertonani@gmail.com>
2021-11-17 17:57:12 -0300

    Implementada os metodos findAll, findById, findByName

--- src/repositories/UserRepository.ts
+++ src/repositories/UserRepository.ts
@@ -2,6 +2,24 @@ import { User } from "../models/UserEntity";
 import { EntityRepository, Repository } from "typeorm";
 import { IUserRepository } from "../@types/repositories/IUserRepository";
 
+
+
 @EntityRepository(User)
 export class UserRepository extends Repository<User> implements IUserRepository{
+  public async findAll(): Promise<User[]> {
+    return await this.find();
+  }
+ public async findById(id: number): Promise<User> {
+   return await this.findOne(id);
+  }
+
+  public async findByName(firstName: string, lastName:string):Promise<User[]> {
+    let resultUsers = await this.find({select: ["firstName","lastName"]});
+
+    const result = resultUsers.filter((result)=>{
+      return result.firstName === firstName && result.lastName === lastName;
+     });
+
+   return result ;
+  }
 }
