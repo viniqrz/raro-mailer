27e29349b793a1c4b8bf2dc39a657eefb4dd242a
Vinicius <viniderp@gmail.com>
2021-11-17 20:25:25 -0300

    remove boilerplate from userservice

--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -3,32 +3,32 @@ import { UserDTO } from "../@types/dto/UserDto";
 import { IUserService } from "../@types/services/IUserService";
 import { IUserRepository } from "../@types/repositories/IUserRepository";
 
-@Service('UserService')
-export class UserService implements IUserService {
-  constructor(@Inject('UserRepository') private userRepository: IUserRepository) {}
+// @Service('UserService')
+// export class UserService implements IUserService {
+//   constructor(@Inject('UserRepository') private userRepository: IUserRepository) {}
 
-  async listar() {
-    return this.userRepository.find();
-  }
+//   async listar() {
+//     return this.userRepository.find();
+//   }
 
-  async buscar(id: number) {
-    return this.userRepository.findOne(id);
-  }
+//   async buscar(id: number) {
+//     return this.userRepository.findOne(id);
+//   }
 
-  async criar(userDto: UserDTO) {
-    return this.userRepository.save(userDto);
-  }
+//   async criar(userDto: UserDTO) {
+//     return this.userRepository.save(userDto);
+//   }
 
-  async atualizar(id: number, userDto: UserDTO) {
-    await this.userRepository.save({...userDto, id});
-  }
+//   async atualizar(id: number, userDto: UserDTO) {
+//     await this.userRepository.save({...userDto, id});
+//   }
 
-  async remover(id: number) {
-    const userToRemove = await this.userRepository.findOne(id);
-    if (!userToRemove) {
-      throw new Error('User not found!');
-    }
+//   async remover(id: number) {
+//     const userToRemove = await this.userRepository.findOne(id);
+//     if (!userToRemove) {
+//       throw new Error('User not found!');
+//     }
 
-    await this.userRepository.remove(userToRemove);
-  }
-}
+//     await this.userRepository.remove(userToRemove);
+//   }
+// }
