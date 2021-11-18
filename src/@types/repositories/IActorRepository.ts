b5bce556281abd06579f3cd5efb1b93b5fda74e5
Rogerio Cezar Fertonani <rogeriocfertonani@gmail.com>
2021-11-18 15:11:09 -0300

    implementar-repositoritorio-de-actor

--- src/@types/repositories/IActorRepository.ts
+++ src/@types/repositories/IActorRepository.ts
@@ -1,9 +1,11 @@
+
 import { Actor } from '../../models/ActorEntity';
+import { ActorDTO } from '../dto/ActorDTO';
 
 
 export interface IActorRepository {
 
-  save(actor:Actor):Promise<Actor>;
+  save(actorDTO:ActorDTO):Promise<Actor>;
   remove(entity:Actor| Actor[]):Promise<Actor[]>;
   findAll():Promise<Actor[]>;
   findById(id:number):Promise<Actor>;
