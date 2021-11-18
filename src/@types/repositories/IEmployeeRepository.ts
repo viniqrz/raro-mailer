026efdf892431a642236571a8b272739873e3399
Rogerio Cezar Fertonani <43325783+rogeriocezarfertonani@users.noreply.github.com>
2021-11-18 14:20:04 -0300

    Identação corrigida, com dois espaços

--- src/@types/repositories/IEmployeeRepository.ts
+++ src/@types/repositories/IEmployeeRepository.ts
@@ -1,10 +1,7 @@
 import { Employee } from '../../models/EmployeeEntity';
 export interface IEmployeeRepository {
-
-save(employee:Employee):Promise<Employee>;
-remove(Employee:Employee):Promise<Employee>;
-findAll():Promise<Employee[]>;
-findByid(id:number):Promise<Employee>;
-
-
+  save(employee:Employee):Promise<Employee>;
+  remove(Employee:Employee):Promise<Employee>;
+  findAll():Promise<Employee[]>;
+  findByid(id:number):Promise<Employee>;
 }
