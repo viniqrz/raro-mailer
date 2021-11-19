6383bf33ba9376a13f3e9594ee9a509190457154
Vinicius <viniderp@gmail.com>
2021-11-19 20:00:25 -0300

    update

--- src/clients/CepClient.ts
+++ src/clients/CepClient.ts
@@ -4,20 +4,20 @@ import { EnderecoDto } from "../@types/dto/EnderecoDto";
 import { ICepClient } from "../@types/clients/ICepClient";
 import { HttpClient } from "../infra/http/types/HttpClient";
 
-@Service('CepClient')
+@Service("CepClient")
 export class CepClient implements ICepClient {
   private API_CEP = `${process.env.BASE_API_CEP}/[CEP]/json/`;
-  constructor(@Inject('HttpClient') private httpClient: HttpClient) {}
+  constructor(@Inject("HttpClient") private httpClient: HttpClient) {}
 
   async buscaEnderecoPorCEP(cep: string): Promise<EnderecoDto> {
-    const url = this.API_CEP.replace('[CEP]', cep);
+    const url = this.API_CEP.replace("[CEP]", cep);
     const response = await this.httpClient.get<EnderecoDto>(url);
     const endereco = response.data;
+
     if (!endereco.cep) {
       throw new CepNaoEncontrado();
     }
 
     return response.data;
   }
-
 }
