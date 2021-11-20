c47d09cd37b6d532738bd3ae97f43beb041d2533
Vinicius <viniderp@gmail.com>
2021-11-19 21:14:47 -0300

    Implementação do Service de EmailTemplate

---
+++ src/@types/services/IEmailTemplateService.ts
@@ -0,0 +1,17 @@
+import {
+  EmailTemplateDTO,
+  UpdateEmailTemplateDTO,
+} from "../dto/EmailTemplateDto";
+
+import { EmailTemplate } from "models/EmailTemplateEntity";
+
+export interface IEmailTemplateService {
+  create(EmailTemplateDto: EmailTemplateDTO): Promise<EmailTemplate>;
+  getById(id: number): Promise<EmailTemplate>;
+  getAll(): Promise<EmailTemplate[]>;
+  update(
+    id: number,
+    EmailTemplateDto: UpdateEmailTemplateDTO
+  ): Promise<EmailTemplate>;
+  delete(id: number): Promise<EmailTemplate>;
+}
