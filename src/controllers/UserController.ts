bbcbc7d63ada7ffdf70af2e802620db371019471
Vinicius <viniderp@gmail.com>
2021-11-19 21:47:04 -0300

    Implementação do Controller de User

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,34 +1,106 @@
 import { Inject, Service } from "typedi";
-import {Request, Response} from "express";
+import { NextFunction, Response } from "express";
 import { IUserService } from "../@types/services/IUserService";
+import { IRequest } from "../@types/express/request";
 
-@Service('UserController')
+@Service("UserController")
 export class UserController {
+  constructor(@Inject("UserService") private userService: IUserService) {}
 
-  constructor(@Inject('UserService') private  userService: IUserService) {}
+  public async signup(req: IRequest, res: Response, next: NextFunction) {
+    try {
+      const { user } = req.body;
 
-  async list(request: Request, response: Response) {
-    const users = await this.userService.listar();
-    response.send(users);
+      const userWithoutPassword = await this.userService.signup(user);
+
+      res.status(200).json({
+        status: "success",
+        data: {
+          userWithoutPassword,
+        },
+      });
+    } catch (err) {
+      next(err);
+    }
   }
 
-  async get(request: Request, response: Response) {
-    const user = await this.userService.buscar(Number(request.params.id));
-    response.send(user);
+  public async authenticate(req: IRequest, res: Response, next: NextFunction) {
+    try {
+      const { email, password } = req.body;
+
+      const { user, token } = await this.userService.authenticate(
+        email,
+        password
+      );
+
+      res.status(200).json({
+        status: "success",
+        data: {
+          user,
+          token,
+        },
+      });
+    } catch (err) {
+      next(err);
+    }
   }
 
-  async create(request: Request, response: Response) {
-    const user = await this.userService.criar(request.body);
-    response.send(user);
+  public async getAll(req: IRequest, res: Response, next: NextFunction) {
+    try {
+      const users = await this.userService.getAll();
+
+      res.status(200).json({
+        status: "success",
+        data: users,
+      });
+    } catch (err) {
+      next(err);
+    }
   }
 
-  async update(request: Request, response: Response) {
-    const user = await this.userService.atualizar(Number(request.params.id), request.body);
-    response.send(user);
+  public async getById(req: IRequest, res: Response, next: NextFunction) {
+    try {
+      const { userId } = req.params;
+
+      const user = await this.userService.getById(Number(userId));
+
+      res.status(200).json({
+        status: "success",
+        data: { user },
+      });
+    } catch (err) {
+      next(err);
+    }
   }
 
-  async remove(request: Request, response: Response) {
-    await this.userService.remover(Number(request.params.id));
-    response.send();
+  public async update(req: IRequest, res: Response, next: NextFunction) {
+    try {
+      const { userId } = req.params;
+      const { user } = req.body;
+
+      const updatedUser = await this.userService.update(Number(userId), user);
+
+      res.status(200).json({
+        status: "success",
+        data: { user: updatedUser },
+      });
+    } catch (err) {
+      next(err);
+    }
+  }
+
+  public async delete(req: IRequest, res: Response, next: NextFunction) {
+    try {
+      const { userId } = req.params;
+
+      const user = await this.userService.delete(Number(userId));
+
+      res.status(200).json({
+        status: "success",
+        data: { user },
+      });
+    } catch (err) {
+      next(err);
+    }
   }
 }
